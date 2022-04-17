994a4179e83bfe854d98f907ce3d4bb33e5a6335
olrahu <olrahu@ttu.ee>
2022-04-17 14:41:25 +0300

    L3

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            url: 'www.google.com',
+            firstImage: './assets/images/socks_green.jpg',
+            secondImage: './assets/images/socks_blue.jpg'
         }
     }
 })
