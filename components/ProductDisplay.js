631eedd0d7e0be7d3103b78374dd2c3c8c5c246e
olrahu <olrahu@ttu.ee>
2022-04-17 19:21:41 +0300

    L11

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -41,7 +41,13 @@ app.component('product-display', {
 
       </div>
     </div>
+
+    <review-list v-if="reviews.length" :reviewList="reviews"></review-list>
+
+    <review-form @review-submitted="addReview"></review-form>
+
   </div>`,
+
   data() {
     return {
         product: 'Socks',
@@ -51,7 +57,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: []
     }
   },
   methods: {
@@ -60,7 +67,10 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
-      }
+      },
+      addReview(review) {
+        this.reviews.push(review);
+    },
   },
   computed: {
       title() {
