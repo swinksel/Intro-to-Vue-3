0dcbeeba40cb2c3e75afd44261eca4b5fbd96ae3
olrahu <olrahu@ttu.ee>
2022-04-17 14:47:17 +0300

    L3 updated url

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            url: 'www.google.com',
+            url: 'https://www.google.com',
             firstImage: './assets/images/socks_green.jpg',
             secondImage: './assets/images/socks_blue.jpg'
         }
